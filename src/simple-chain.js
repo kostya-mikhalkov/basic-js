7158e37559adb558cb91e593009041d0d8a5abdd
Kostya <kostya.mikhalkov@bk.ru>
2023-12-18 22:13:20 +0300

    feat: complete simpleChain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,28 +2,33 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  stack: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.stack.push(`( ${value !== undefined ? value : ''} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(position < 1 || position > this.stack.length || !Number.isInteger(position)) {
+      this.stack = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.stack.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.stack.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const res = this.stack.join('~~');
+    this.stack = [];
+    return res;
   }
 };
 
