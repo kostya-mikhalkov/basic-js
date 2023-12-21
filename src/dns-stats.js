5a67cac84a8e615f6f097a16787fdfd84a2d0fb7
Kostya <kostya.mikhalkov@bk.ru>
2023-12-21 20:38:01 +0300

    feat: complete dnsStats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {};
+  for(let i = 0; i < domains.length; i++) {
+    let subDomain = domains[i].split('.').reverse();
+    let currentDns = '';
+
+    for(let j = 0; j < subDomain.length; j++) {
+      currentDns += `.${subDomain[j]}`;
+      if(!obj[currentDns]) {
+        obj[currentDns] = 1;
+      }else{
+        obj[currentDns]++;
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = {
